30ab003daa810baa511e07ab66030964a2a9b7b7
shadow_walker <shadowwalkersb@gmail.com>
2016-02-14 17:32:22 -0600

    working

--- src/toolkit/MathTools/Dims.h
+++ src/toolkit/MathTools/Dims.h
@@ -5,47 +5,47 @@
 using namespace std;
 
 template <int D, class T>
-class Dim {
+class DimBase {
   public:
-    Dim()    : dim(T()), dims()  {}
-    Dim(T v) : dim(v),   dims(v-1) {
-      cout<<"Dim<"<<D<<">::ctor("<<v<<")"<<endl;
+    DimBase()    : dim(T()), dims()  {}
+    DimBase(T v) : dim(v),   dims(v-1) {
+      cout<<"DimBase<"<<D<<">::ctor("<<v<<")"<<endl;
     }
 
     T& operator[](int d) {
       //cout<<"operator[]<"<<D<<">(int "<<d<<")"<<endl;
-      return const_cast<T &>(static_cast<const Dim<D,T>& >(*this)[d]);
+      return const_cast<T &>(static_cast<const DimBase<D,T>& >(*this)[d]);
     }
-    
+
     const T& operator[](int d) const {
       //cout<<"operator[]<"<<D<<">(int "<<d<<")"<<endl;
       return d==D ? dim : dims[d];
     }
 
     T dim;
-    Dim<D-1,T> dims;
+    DimBase<D-1,T> dims;
 };
 
 template <int D, class T>
-ostream& operator<<(ostream& out, const Dim<D,T> &obj){
+ostream& operator<<(ostream& out, const DimBase<D,T> &obj){
     out<<obj.dims
 	<<" "<<obj.dim;
-  
+
   return out;
 }
 
 template <class T>
-class Dim<1,T> {
+class DimBase<1,T> {
   public:
-    Dim()    : dim(T()) {}
-    Dim(T v) : dim(v)   {
-      cout<<"Dim<1>::ctor("<<v<<")"<<endl;
+    DimBase()    : dim(T()) {}
+    DimBase(T v) : dim(v)   {
+      cout<<"DimBase<1>::ctor("<<v<<")"<<endl;
     }
 
     T& operator[](int d) {
       //cout<<"operator[]<"<<1<<">(int "<<d<<")"<<endl;
       //return dim;
-      return const_cast<T &>(static_cast<const Dim<1,T>& >(*this)[d]);
+      return const_cast<T &>(static_cast<const DimBase<1,T>& >(*this)[d]);
     }
 
     const T& operator[](int d) const {
@@ -56,8 +56,30 @@ class Dim<1,T> {
 };
 
 template <class T>
-ostream& operator<<(ostream& out, const Dim<1,T> &obj){
+ostream& operator<<(ostream& out, const DimBase<1,T> &obj){
     out<<obj.dim;
-  
+
   return out;
 }
+
+template <int D, class T>
+class Dim : public DimBase<D,T>{
+    public:
+        Dim()    : DimBase<D,T>() {}
+        Dim(T v) : DimBase<D,T>(v)   {}
+};
+
+template <class T>
+class Dim<1,T> : public DimBase<1,T>{
+    public:
+        Dim()    : DimBase<1,T>() {}
+        Dim(T v) : DimBase<1,T>(v)   {}
+};
+
+//template<class T>
+//class DimBase<1,T> {
+//  public:
+//    T Y() {
+//      return this->operator[](1);
+//    }
+
