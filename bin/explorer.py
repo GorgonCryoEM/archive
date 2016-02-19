dd3ca2667d0e745738c773a26381a5fb7cd00cf9
shadow_walker <shadowwalkersb@gmail.com>
2016-02-18 22:04:45 -0600

    fix tabs, whitespace

--- bin/explorer.py
+++ bin/explorer.py
@@ -9,39 +9,40 @@ from OpenGL.GLUT import *
 import sys
 import argparse
 
+
 class GLWidget(QtOpenGL.QGLWidget):
+
     def __init__(self):
         super(GLWidget, self).__init__()
         
         self.setMinimumSize(300, 300)
         self.setWindowTitle('Gorgon Explorer - alpha')
         
-	def initializeGL(self):
-		glEnable(GL_DEPTH_TEST)
+    def initializeGL(self):
+        glEnable(GL_DEPTH_TEST)
 
-	def paintGL(self):	
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-		glClearColor(0,0,0,1)
-		
-	def resizeGL(self, w, h):
-		glViewport(0,0,w,h)
-		
-
+    def paintGL(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glClearColor(0,0,0,1)
+        
+    def resizeGL(self, w, h):
+        glViewport(0,0,w,h)
+        
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Gorgon Explorer')
-	
-	parser.add_argument('volume', action="store")
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Gorgon Explorer')
+    
+    parser.add_argument('volume', action="store")
+    args = parser.parse_args()
 
-	app = QtGui.QApplication(sys.argv)    
+    app = QtGui.QApplication(sys.argv)
 
-	window = GLWidget()
-	window.showNormal()
-	window.raise_()
-	window.resize(800, 800)
-	window.move(300,50)
-	
-	app.exec_()
+    window = GLWidget()
+    window.showNormal()
+    window.raise_()
+    window.resize(800, 800)
+    window.move(300,50)
+    
+    app.exec_()
 
-	print args.volume
+    print args.volume
