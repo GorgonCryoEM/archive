1218a981ab2dd1ef35534ce631d20f776c9c3b81
shadow_walker <shadowwalkersb@gmail.com>
2016-02-18 23:40:26 -0600

    cleanup copyright, remove namespace, include guards

--- src/toolkit/Visualization/TriangleMeshVertex.h
+++ src/toolkit/Visualization/TriangleMeshVertex.h
@@ -1,34 +1,36 @@
-// Copyright (C) 2005-2008 Washington University in St Louis, Baylor College of Medicine.  All rights reserved
-// Author:        Sasakthi S. Abeysinghe (sasakthi@gmail.com)
-// Description:   A class for vertex stored in a triangle mesh
+/*
+ * TriangleMeshVertex.h
+ *
+ * Author: shadow_walker <shadowwalkersb@gmail.com>
+ *
+ */
 
 
+#ifndef SRC_TOOLKIT_VISUALIZATION_TRIANGLEMESHVERTEX_H_
+#define SRC_TOOLKIT_VISUALIZATION_TRIANGLEMESHVERTEX_H_
 
-#ifndef PROTEINMORPH_TRIANGLE_MESH_VERTEX_H
-#define PROTEINMORPH_TRIANGLE_MESH_VERTEX_H
 
 using namespace std;
 
-namespace wustl_mm {
-	namespace Protein_Morph {
-		template <class TVertex> class TriangleMeshVertex {
-		public:
-			TriangleMeshVertex();
-			TriangleMeshVertex(Vector3DFloat position);
-		public:
-			Vector3DFloat position;
-			vector<unsigned long long> faceHashes;
-			TVertex tag;
-		};		
+namespace Protein_Morph {
+    template <class TVertex> class TriangleMeshVertex {
+    public:
+        TriangleMeshVertex();
+        TriangleMeshVertex(Vector3DFloat position);
+    public:
+        Vector3DFloat position;
+        vector<unsigned long long> faceHashes;
+        TVertex tag;
+    };
 
-		template <class TVertex> TriangleMeshVertex<TVertex>::TriangleMeshVertex() {
-			position = Vector3DFloat(0, 0, 0);
-		}
+    template <class TVertex> TriangleMeshVertex<TVertex>::TriangleMeshVertex() {
+        position = Vector3DFloat(0, 0, 0);
+    }
 
-		template <class TVertex> TriangleMeshVertex<TVertex>::TriangleMeshVertex(Vector3DFloat position) {
-			this->position = position;
-		}
-	}
+    template <class TVertex> TriangleMeshVertex<TVertex>::TriangleMeshVertex(Vector3DFloat position) {
+        this->position = position;
+    }
 }
 
-#endif
+
+#endif /* SRC_TOOLKIT_VISUALIZATION_TRIANGLEMESHVERTEX_H_ */
