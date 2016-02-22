e69c8d0bb9b887f1cde1a4495ced2e29992660cc
shadow_walker <shadowwalkersb@gmail.com>
2016-02-21 19:47:39 -0600

    reset

--- bin/Explorer/__init__.py
+++ bin/Explorer/__init__.py
@@ -1,2 +1,9 @@
-from volume_viewer import VolumeViewer
-from camera import Camera
+import sys, os, inspect
+
+from PyQt4 import QtGui, QtCore
+from main_window_form import MainWindowForm
+
+app = QtGui.QApplication(sys.argv)    
+app.processEvents()
+
+window = MainWindowForm(__version__)
