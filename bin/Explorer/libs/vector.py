e1e073556c3c49f233cff243832c67febb1d95ac
shadow_walker <shadowwalkersb@gmail.com>
2016-02-22 15:34:05 -0600

    class Vector

--- bin/Explorer/libs/vector.py
+++ bin/Explorer/libs/vector.py
@@ -5,6 +5,33 @@
 
 from cmath import *   
     
+class Vector:
+	def __init__(self, v):
+		self.v = [v[0], v[1], v[2]]
+		
+	def len(self):
+		return abs(sqrt(self.v[0]*self.v[0] + self.v[1]*self.v[1] + self.v[2]*self.v[2]))
+	
+	def __mul__(self, s):
+		return [s*self.v[0], s*self.v[1], s*self.v[2]]
+	
+	def __add__(self, v1):
+		return [self.v[0]+v1[0], self.v[1]+v1[1], self.v[2]+v1[2]]
+	
+	def __sub__(self, v1):
+		return [self.v[0]+v1[0], self.v[1]+v1[1], self.v[2]+v1[2]]
+	
+	def normalize(self):
+		base = self.len()
+		if(base == 0) :
+			return self.v
+		else :
+			return self.__mul__(base)
+		
+	def __getitem__(self, i):
+		return self.v[i]
+
+		    
 def vectorDistance(v1, v2):
     return abs(sqrt(pow(v1[0]-v2[0], 2) + pow(v1[1]-v2[1], 2) + pow(v1[2]-v2[2], 2)))
         
