260ffc406449fa4e9df0e3c7cf477f5a29227780
shadow_walker <shadowwalkersb@gmail.com>
2016-02-21 22:11:53 -0600

    save

--- bin/Explorer/base_viewer.py
+++ bin/Explorer/base_viewer.py
@@ -309,7 +309,7 @@ class BaseViewer(QtOpenGL.QGLWidget):
         
         self.setCursor(QtCore.Qt.ArrowCursor)
         
-    def saveData(self,fileName):
+    def save(self,fileName):
         self.setCursor(QtCore.Qt.WaitCursor)
         self.renderer.saveFile(str(fileName))
         self.dirty = False
