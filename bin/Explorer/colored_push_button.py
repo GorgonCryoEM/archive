53a11aaf67f0225780b48d16c04f1154295821c8
shadow_walker <shadowwalkersb@gmail.com>
2016-02-21 23:39:04 -0600

    remove brush

--- bin/Explorer/colored_push_button.py
+++ bin/Explorer/colored_push_button.py
@@ -6,9 +6,6 @@ class ColoredPushButton(QtGui.QPushButton):
     def __init__(self, parent = None):
         QtGui.QWidget.__init__(self, parent)
         self.actualColor = QtGui.QColor.fromRgba(QtGui.qRgba(128, 128, 128, 255))
-        self.brush = QtGui.QBrush()
-        self.brush.setColor(self.actualColor)
-        self.brush.setStyle(QtCore.Qt.SolidPattern)
         
         self.connect(self, QtCore.SIGNAL("pressed ()"), self.buttonPressed)
         self.colorPicker = QtGui.QColorDialog(self)
@@ -17,13 +14,12 @@ class ColoredPushButton(QtGui.QPushButton):
         QtGui.QPushButton.paintEvent(self, event)
         painter = QtGui.QPainter()
         painter.begin(self)
-        painter.fillRect(5, 4, self.width()-10, self.height()-8, self.brush)
+        painter.fillRect(5, 4, self.width()-10, self.height()-8, self.actualColor)
         painter.end()
     
     def setColor(self, color):
         if (self.actualColor != color):
             self.actualColor = color;
-            self.brush.setColor(QtGui.QColor.fromRgba(QtGui.qRgba(color.red(), color.green(), color.blue(), 255)))
             self.update()
     
     def color(self):
