43cbd698306efbdf1a4d2a5d4182965fdabcd8e2
shadow_walker <shadowwalkersb@gmail.com>
2016-02-22 17:17:48 -0600

    deactivate vector lib

--- bin/Explorer/libs/vector.py
+++ bin/Explorer/libs/vector.py
@@ -1,30 +1,30 @@
 from cmath import *   
 from libpytoolkit import *
     
-def vectorDistance(v1, v2):
-    return vectorSubtract(v1,v2).length()
-        
-def vectorSize(v):        
-    return Vector3Float(v[0], v[1], v[2]).length()
-        
-def vectorNormalize(v):
-	v = Vector3Float(v[0], v[1], v[2])
-	v.normalize()
-	
-	return v
-    
-def vectorCrossProduct(v1, v2):
-    return Vector3Float(v1[1] * v2[2] - v1[2] * v2[1], v1[2] * v2[0] - v1[0] * v2[2], v1[0] * v2[1] - v1[1] * v2[0])
-
-def vectorAdd(v1, v2):
-	v = Vector3Float(v1[0], v1[1], v1[2]) + Vector3Float(v2[0], v2[1], v2[2])
-	return v
-    
-
-def vectorSubtract(v1, v2):
-	v = Vector3Float(v1[0], v1[1], v1[2]) - Vector3Float(v2[0], v2[1], v2[2])
-	return v
-
-def vectorScalarMultiply(s, v):
-	v = Vector3Float(v[0], v[1], v[2])*s
-	return v
+# def vectorDistance(v1, v2):
+#     return vectorSubtract(v1,v2).length()
+#         
+# def vectorSize(v):        
+#     return Vector3Float(v[0], v[1], v[2]).length()
+#         
+# def vectorNormalize(v):
+# 	v = Vector3Float(v[0], v[1], v[2])
+# 	v.normalize()
+# 	
+# 	return v
+#     
+# def vectorCrossProduct(v1, v2):
+#     return Vector3Float(v1[1] * v2[2] - v1[2] * v2[1], v1[2] * v2[0] - v1[0] * v2[2], v1[0] * v2[1] - v1[1] * v2[0])
+# 
+# def vectorAdd(v1, v2):
+# 	v = Vector3Float(v1[0], v1[1], v1[2]) + Vector3Float(v2[0], v2[1], v2[2])
+# 	return v
+#     
+# 
+# def vectorSubtract(v1, v2):
+# 	v = Vector3Float(v1[0], v1[1], v1[2]) - Vector3Float(v2[0], v2[1], v2[2])
+# 	return v
+# 
+# def vectorScalarMultiply(s, v):
+# 	v = Vector3Float(v[0], v[1], v[2])*s
+# 	return v
