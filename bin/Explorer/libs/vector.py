53012b8cbf95611ffe8eb7539169c1de4c767b0c
shadow_walker <shadowwalkersb@gmail.com>
2016-02-22 16:14:30 -0600

    FIX

--- bin/Explorer/libs/vector.py
+++ bin/Explorer/libs/vector.py
@@ -10,22 +10,26 @@ def vectorDistance(v1, v2):
     return abs(sqrt(pow(v1[0]-v2[0], 2) + pow(v1[1]-v2[1], 2) + pow(v1[2]-v2[2], 2)))
         
 def vectorSize(v):        
-    return Vector3Float(v).length()
+    return Vector3Float(v[0], v[1], v[2]).length()
         
 def vectorNormalize(v):
-	v = Vector3Float(v[0], v[1], v[2]).normalize()
-	return [v[0], v[1], v[2]]
+	v = Vector3Float(v[0], v[1], v[2])
+	v.normalize()
+	return [v.x(), v.y(), v.z()]
     
 def vectorCrossProduct(v1, v2):
     return [v1[1] * v2[2] - v1[2] * v2[1], v1[2] * v2[0] - v1[0] * v2[2], v1[0] * v2[1] - v1[1] * v2[0]]
 
 def vectorAdd(v1, v2):
-	return Vector(v1) + Vector(v2)
+	v = Vector3Float(v1[0], v1[1], v1[2]) + Vector3Float(v2[0], v2[1], v2[2])
+	return [v.x(), v.y(), v.z()]
     
 
 def vectorSubtract(v1, v2):
-    return Vector(v1) - Vector(v2)
+	v = Vector3Float(v1[0], v1[1], v1[2]) - Vector3Float(v2[0], v2[1], v2[2])
+	return [v.x(), v.y(), v.z()]
 
 def vectorScalarMultiply(s, v):
-# 		return s*Vector(v)
-	return Vector(v)*float(s)
+	v = Vector3Float(v[0], v[1], v[2])*s
+# 		return s*Vector3Float(v)
+	return [v.x(), v.y(), v.z()]
