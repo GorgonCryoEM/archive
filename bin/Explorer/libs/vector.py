81a9c343c3b5a910e6622806300d45c05a2e033a
shadow_walker <shadowwalkersb@gmail.com>
2016-02-22 16:49:57 -0600

    cleanup copyrights

--- bin/Explorer/libs/vector.py
+++ bin/Explorer/libs/vector.py
@@ -1,8 +1,3 @@
-# Copyright (C) 2005-2008 Washington University in St Louis, Baylor College of Medicine.  All rights reserved
-# Author:        Sasakthi S. Abeysinghe (sasakthi@gmail.com)
-# Description:   A utility module for vector manipulation 
-
-
 from cmath import *   
 from libpytoolkit import *
     
