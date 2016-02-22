8a1bb80e39f8b49c1f467a582bbad3fef3804dc8
shadow_walker <shadowwalkersb@gmail.com>
2016-02-22 15:34:21 -0600

    refactor lib vector

--- bin/Explorer/libs/vector.py
+++ bin/Explorer/libs/vector.py
@@ -36,23 +36,21 @@ def vectorDistance(v1, v2):
     return abs(sqrt(pow(v1[0]-v2[0], 2) + pow(v1[1]-v2[1], 2) + pow(v1[2]-v2[2], 2)))
         
 def vectorSize(v):        
-    return abs(sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]))
+    return Vector(v).len()
         
 def vectorNormalize(v):
-    base = abs(sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]))
-    if(base == 0) :
-        return v
-    else :
-        return [float(v[0]/base), float(v[1]/base), float(v[2]/base)]
+    return Vector(v).normalize()
     
 def vectorCrossProduct(v1, v2):
     return [v1[1] * v2[2] - v1[2] * v2[1], v1[2] * v2[0] - v1[0] * v2[2], v1[0] * v2[1] - v1[1] * v2[0]]
 
 def vectorAdd(v1, v2):
-    return [v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]]
+	return Vector(v1) + Vector(v2)
+    
 
 def vectorSubtract(v1, v2):
-    return [v1[0] - v2[0], v1[1] - v2[1], v1[2] - v2[2]]
+    return Vector(v1) - Vector(v2)
 
 def vectorScalarMultiply(s, v):
-    return [s*v[0], s*v[1], s*v[2]]
+# 		return s*Vector(v)
+	return Vector(v)*float(s)
