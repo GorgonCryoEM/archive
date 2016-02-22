eca5427d65150ad00ecd341c0ee6107b70dbcfd0
shadow_walker <shadowwalkersb@gmail.com>
2016-02-21 20:50:29 -0600

    cleanup __init__

--- bin/Explorer/__init__.py
+++ bin/Explorer/__init__.py
@@ -1,9 +1,3 @@
-import sys, os, inspect
-
-from PyQt4 import QtGui, QtCore
 from main_window_form import MainWindowForm
 
-app = QtGui.QApplication(sys.argv)    
-app.processEvents()
-
-window = MainWindowForm(__version__)
+window = MainWindowForm('2.2.3')
