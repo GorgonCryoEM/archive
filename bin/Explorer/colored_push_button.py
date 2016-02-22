a01d8d3d30cf16e0d37f8215ea7322ef4c786f37
shadow_walker <shadowwalkersb@gmail.com>
2016-02-21 23:44:45 -0600

    setColor

--- bin/Explorer/colored_push_button.py
+++ bin/Explorer/colored_push_button.py
@@ -16,13 +16,10 @@ class ColoredPushButton(QtGui.QPushButton):
         painter.fillRect(5, 4, self.width()-10, self.height()-8, self.actualColor)
         painter.end()
     
-    def setColor(self, color):
-        self.actualColor = color;
-        self.update()
-    
     def buttonPressed(self):
         self.colorPicker.setCurrentColor(self.actualColor)
-        self.setColor(self.colorPicker.getColor())
+        self.actualColor = self.colorPicker.getColor()
+        self.update()
         self.emitColorChanged()
     
     def emitColorChanged(self):
