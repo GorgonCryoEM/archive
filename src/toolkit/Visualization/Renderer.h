da70fc85cc7ab2299f13f0e382167c0447c47d3f
shadow_walker <shadowwalkersb@gmail.com>
2016-02-24 10:17:20 -0600

    includes

--- src/toolkit/Visualization/Renderer.h
+++ src/toolkit/Visualization/Renderer.h
@@ -11,7 +11,7 @@
 //#include <GorgonGL.h>
 #include "OpenGLUtils.h"
 //#include <MathTools/Vector3D.h>
-//#include <MathTools/MathLib.h>
+#include <MathTools/MathLib.h>
 #include "MathTools/Dim3D.h"
 
 //using namespace std;
