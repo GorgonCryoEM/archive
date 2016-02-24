6f5077445dd6a74945f3c6749427db906dcb0752
shadow_walker <shadowwalkersb@gmail.com>
2016-02-23 22:01:28 -0600

    cleanup

--- bin/Explorer/libs/vector_3_float.py
+++ bin/Explorer/libs/vector_3_float.py
@@ -2,26 +2,8 @@ from libpytoolkit import Vector3Float as Vec3F
 
 
 class Vec3(Vec3F):
-#     pass
-
-#     def __init__(self):
-#         print "I am in : ", __name__
-#         super(Vec3, self).__init__(0.0,0.0,0.0)
         
     def __init__(self, *args, **kwargs):
-#         print "args: ", args
-#         print "kwargs: ", kwargs
-#
-#         if args:
-#             print "args"
-#         if not args:
-#             print "args not"
-#
-#         if kwargs:
-#             print "kwargs"
-#         if not kwargs:
-#             print "kwargs not"
-        
         if kwargs:
             raise Exception("Wrong named-arguments")
         if len(args) == 1 and (isinstance(args[0], list) or isinstance(args[0], Vec3F)):
@@ -30,11 +12,5 @@ class Vec3(Vec3F):
             [x, y, z] = [args[i] for i in range(3)]
         else:
             raise Exception("Wrong arguments:\nargs:%s\n%s" %(args, kwargs))
-#         if len(args) == 1 and not isinstance(args[1], list):
-#             raise Exception("Not a list")
 
-#         [x, y, z] = [args[i] for i in range(3)]
         super(Vec3, self).__init__(x, y, z)
-
-#     def __init__(self, x, y, z):
-#         super(Vec3, self).__init__(x, y, z)
