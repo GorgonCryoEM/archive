d008b1995b948e839729bb00ddbdf1317ec66bcf
shadow_walker <shadowwalkersb@gmail.com>
2016-02-23 21:40:11 -0600

    refactor Vec3

--- bin/Explorer/libs/vector_3_float.py
+++ bin/Explorer/libs/vector_3_float.py
@@ -8,8 +8,32 @@ class Vec3(Vec3F):
 #         print "I am in : ", __name__
 #         super(Vec3, self).__init__(0.0,0.0,0.0)
         
-    def __init__(self, l):
-        [x, y, z] = [l[i] for i in range(3)]
+    def __init__(self, *args, **kwargs):
+#         print "args: ", args
+#         print "kwargs: ", kwargs
+#
+#         if args:
+#             print "args"
+#         if not args:
+#             print "args not"
+#
+#         if kwargs:
+#             print "kwargs"
+#         if not kwargs:
+#             print "kwargs not"
+        
+        if kwargs:
+            raise Exception("Wrong named-arguments")
+        if len(args) == 1 and isinstance(args[0], list):
+            [x, y, z] = args[0]
+        elif len(args) == 3:
+            [x, y, z] = [args[i] for i in range(3)]
+        else:
+            raise Exception("Wrong arguments")
+#         if len(args) == 1 and not isinstance(args[1], list):
+#             raise Exception("Not a list")
+
+#         [x, y, z] = [args[i] for i in range(3)]
         super(Vec3, self).__init__(x, y, z)
 
 #     def __init__(self, x, y, z):
