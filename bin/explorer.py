c401172cedf0ee9c2619cbc9f3cd85734fb2ad2d
shadow_walker <shadowwalkersb@gmail.com>
2016-03-08 12:43:51 -0600

    add new window

--- bin/explorer.py
+++ bin/explorer.py
@@ -59,6 +59,13 @@ def main():
 #     gorg.window.skeletonViewer.loadDataFromFile(args.skeleton)
     
     window = MainWindowForm('2.2.3')
+    
+    window1 = MainWindowForm('2.2.4')
+    window1.move(500,50)
+    window1.resize(600, 600)
+    window1.show()
+    window1.volumeViewer.load("vp6-96o.mrc")
+#     window1.skeletonViewer.load(args.skeleton)
 
     window.resize(800, 600)
 #     window.move(300,50)
