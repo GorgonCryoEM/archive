a5b49063669e1c26dbab41a968a2094b814d1ec7
shadow_walker <shadowwalkersb@gmail.com>
2016-03-10 00:34:50 -0600

    no volume_surface_editor

--- bin/Explorer/volume_viewer.py
+++ bin/Explorer/volume_viewer.py
@@ -1,7 +1,6 @@
 from PyQt4 import QtGui, QtCore, QtOpenGL
 from base_viewer import BaseViewer
 from libpytoolkit import Display
-from volume_surface_editor_form import VolumeSurfaceEditorForm
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
@@ -17,4 +16,17 @@ class VolumeViewer(BaseViewer):
 
         self.renderer = Display()
         self.loaded = False
-        self.surfaceEditor = VolumeSurfaceEditorForm(self.app, self, self)
+        
+        self.connect(self, QtCore.SIGNAL("modelLoadedPreDraw()"), self.modelLoadedPreDraw)
+
+    def modelLoadedPreDraw(self):
+        self.renderer.enableDraw(False)
+        maxDensity = self.renderer.getMaxDensity()
+        minDensity = self.renderer.getMinDensity()
+        defaultDensity = (minDensity + maxDensity) / 2
+        
+        maxRadius = int(max(self.renderer.getMax(0)/2, self.renderer.getMax(1)/2, self.renderer.getMax(2)/2));
+        self.renderer.setSampleInterval(1)
+        self.renderer.setSurfaceValue(defaultDensity)
+        self.renderer.setDisplayRadius(maxRadius)
+        self.renderer.enableDraw(True)
