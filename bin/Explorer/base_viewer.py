bf92cac53c029d4c5e0f0116bbc9ad7a07971559
shadow_walker <shadowwalkersb@gmail.com>
2016-03-10 01:19:51 -0600

    setModelColor0

--- bin/Explorer/base_viewer.py
+++ bin/Explorer/base_viewer.py
@@ -39,12 +39,8 @@ class BaseViewer(QtOpenGL.QGLWidget):
         self.modelColor = QtGui.QColor(180, 180, 180, 150)
         
     def setModelColor(self, color):
-        oldColor = self.getModelColor()
-        self.setModelColor0(color)
-        self.repaintCamera()
-
-    def setModelColor0(self, color):
         self.modelColor = color
+        self.repaintCamera()
 
     def identityMatrix(self):
         return [[1.0, 0.0, 0.0, 0.0],
