c92f2071644d9bf9df1e6e19a8b990dbbc38bcdc
shadow_walker <shadowwalkersb@gmail.com>
2016-03-10 01:26:46 -0600

    getModelColor

--- bin/Explorer/base_viewer.py
+++ bin/Explorer/base_viewer.py
@@ -86,9 +86,6 @@ class BaseViewer(QtOpenGL.QGLWidget):
         self.displayStyle = style
         self.emitModelVisualizationChanged()
 
-    def getModelColor(self):
-        return self.modelColor
-
     def setMaterials(self, color):
         glColor4f(color.redF(), color.greenF(), color.blueF(), color.alphaF())
         diffuseMaterial = [color.redF(), color.greenF(), color.blueF(), color.alphaF()]
@@ -176,11 +173,9 @@ class BaseViewer(QtOpenGL.QGLWidget):
         
         self.emitDrawingModel()
         
-        colors     = self.getModelColor()
-                
         for i in range(len(self.glLists)):
             if(self.loaded):
-                self.setMaterials(colors)
+                self.setMaterials(self.modelColor)
                 self.initializeGLDisplayType()
                 glCallList(self.glLists[i])
                 self.unInitializeGLDisplayType();
@@ -208,8 +203,6 @@ class BaseViewer(QtOpenGL.QGLWidget):
             glDeleteLists(list,1)
         self.glLists = []
             
-        colors = self.getModelColor()
-        
         glPushAttrib(GL_LIGHTING_BIT | GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT)
                          
         if(self.loaded):
@@ -217,7 +210,7 @@ class BaseViewer(QtOpenGL.QGLWidget):
             glNewList(list, GL_COMPILE)
             self.glLists.append(list)
 
-            if(colors.alpha() < 255):
+            if(self.modelColor.alpha() < 255):
                 glDepthFunc(GL_LESS)
                 glColorMask(False, False, False, False)
                 self.renderer.draw(0, False)
