0079d92c409a1cc09ae798523651c1521675a2a9
shadow_walker <shadowwalkersb@gmail.com>
2016-03-13 23:07:13 -0500

    line

---
+++ bin/Explorer/line.py
@@ -0,0 +1,46 @@
+from PyQt4 import QtGui, QtCore, QtOpenGL
+from base_viewer import BaseViewer
+from libpytoolkit import Display
+from libpytoolkit import RendererBase
+from libpytoolkit import Vec3F
+from .libs import Vec3
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+
+class Line(BaseViewer):
+    
+    def __init__(self, main, parent=None):
+#         super(Sphere, self).__init__(main, parent)
+        BaseViewer.__init__(self, main, parent)
+        self.title = "Line"
+        
+        self.renderer = Display()
+        self.s = RendererBase()
+#         self.color = QtGui.QColorDialog().getColor()
+        self.color = QtGui.QColor(40, 70, 100, 150)
+        
+        self.main = main
+        
+        self.p1 = Vec3(30., 10., 10.)
+        self.p2 = Vec3( 0.,  0.,  0.)
+        
+        self.selectEnabled    = True
+        self.mouseMoveEnabled = True
+        
+    def draw(self):
+        self.setMaterials(self.color)
+        glLineWidth(5.)
+        glClear(GL_DEPTH_BUFFER_BIT)
+        self.s.drawLine(self.p1, self.p2)
+        glEnable(GL_DEPTH_TEST)
+
+    def selectionMove(self, v):
+        print "     In: selectionMove", self
+        self.p1 += v
+        self.draw()
+        
+    def getCOM(self):
+        return (self.p1+self.p2)/2.
