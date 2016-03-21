b986510dbe2c6ba0af9dd6918da3ebee57e36e05
shadow_walker <shadowwalkersb@gmail.com>
2016-03-21 11:03:41 -0500

    add R

--- bin/Explorer/sphere.py
+++ bin/Explorer/sphere.py
@@ -3,10 +3,11 @@ from shape import Shape
 
 class Sphere(Shape):
     
-    def __init__(self, main):
+    def __init__(self, main, R=1.0):
         super(Sphere, self).__init__(main)
         
         self.title = "Sphere"
+        self.R = R
         
         self.setColor(120, 18, 80, 150)
         
@@ -14,4 +15,4 @@ class Sphere(Shape):
         
     def draw(self):
         self.setMaterials(self.color)
-        self.s.drawSphere(self.loc, 10.)
+        self.s.drawSphere(self.loc, self.R)
