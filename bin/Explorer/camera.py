52c8a8939d9fe2e30fe0c8f7fd55bbf8c7506c82
shadow_walker <shadowwalkersb@gmail.com>
2016-03-21 08:38:10 -0500

    fix selection move and rotate

--- bin/Explorer/camera.py
+++ bin/Explorer/camera.py
@@ -358,30 +358,32 @@ class Camera(QtOpenGL.QGLWidget):
         print "In: moveSelectedShape"
         print "selectedShape: %d" % self.selectedShape
         
-        dirVec = self.mouseVec(dx, dy)
-        
-        s = self.shapes[self.selectedShape]
-        s.selectionMove(dirVec)
+        if self.selectedShape > -1:
+            dirVec = self.mouseVec(dx, dy)
+            
+            s = self.shapes[self.selectedShape]
+            s.selectionMove(dirVec)
 
     def rotateSelectedShape(self, dx, dy):
         print "In: rotateSelectedShape"
         print "selectedShape: %d" % self.selectedShape
-
-        moveLength    = self.mouseVec(dx, dy)
-        dirVec = moveLength.normalize()
-        rotationAxis3D  = dirVec^self.look
         
-        s = self.shapes[self.selectedShape]
-        centerOfMass   = s.getCOM()
-        print "  COM: ", s, centerOfMass
-        centerOfMass.Print()
-        
-        selectionCOM  = centerOfMass
-        selectionAxis = rotationAxis3D
-        axisDraw = selectionAxis*1000.
-        self.lineaxis.redraw(selectionCOM, axisDraw)
-        self.dotcom.loc = selectionCOM
-        s.selectionRotate(selectionCOM, selectionAxis, 5.)
+        if self.selectedShape > -1:
+            moveLength    = self.mouseVec(dx, dy)
+            dirVec = moveLength.normalize()
+            rotationAxis3D  = dirVec^self.look
+            
+            s = self.shapes[self.selectedShape]
+            centerOfMass   = s.getCOM()
+            print "  COM: ", s, centerOfMass
+            centerOfMass.Print()
+            
+            selectionCOM  = centerOfMass
+            selectionAxis = rotationAxis3D
+            axisDraw = selectionAxis*1000.
+            self.lineaxis.redraw(selectionCOM, axisDraw)
+            self.dotcom.loc = selectionCOM
+            s.selectionRotate(selectionCOM, selectionAxis, 5.)
 
     def mousePressEvent(self, event):
         self.mouseDownPoint    = QtCore.QPoint(event.pos())
