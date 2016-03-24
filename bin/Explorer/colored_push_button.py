9569c8a930fcbb73c8aeaa7631f4ed18e71cfde6
shadow_walker <shadowwalkersb@gmail.com>
2016-03-24 12:24:44 -0500

    colored_push_button

---
+++ bin/Explorer/colored_push_button.py
@@ -0,0 +1,34 @@
+from PyQt4 import QtCore, QtGui
+
+
+class ColoredPushButton(QtGui.QPushButton):
+    
+    def __init__(self, parent = None):
+        QtGui.QWidget.__init__(self, parent)
+        self.actualColor = QtGui.QColor.fromRgba(QtGui.qRgba(128, 128, 128, 255))
+        
+        self.connect(self, QtCore.SIGNAL("pressed ()"), self.buttonPressed)
+        self.colorPicker = QtGui.QColorDialog(self)
+        
+    def paintEvent(self, event):
+        painter = QtGui.QPainter()
+        painter.begin(self)
+        painter.fillRect(5, 4, self.width()-10, self.height()-8, self.actualColor)
+        painter.end()
+    
+    def setColor(self, color):
+        self.actualColor = color;
+        self.update()
+    
+    def color(self):
+        return self.actualColor
+
+    def buttonPressed(self):
+        self.colorPicker.setCurrentColor(self.actualColor)
+        self.setColor(self.colorPicker.getColor())
+        self.emitColorChanged()
+    
+    def emitColorChanged(self):
+        self.emit(QtCore.SIGNAL("colorChanged()"))
+
+    
