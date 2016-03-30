5d2490bb124caccc85a7689e4fbc334043abb91e
shadow_walker <shadowwalkersb@gmail.com>
2016-03-30 11:35:10 -0500

    cleanup, tear down

--- bin/Explorer/model_visualization_form.py
+++ bin/Explorer/model_visualization_form.py
@@ -16,38 +16,5 @@ class ModelVisualizationForm(BaseDockWidget):
                                 QtCore.Qt.LeftDockWidgetArea | QtCore.Qt.RightDockWidgetArea | QtCore.Qt.BottomDockWidgetArea,
                                 QtCore.Qt.RightDockWidgetArea)
 
-        self.connect(self.viewer, QtCore.SIGNAL("modelLoaded()"), self.show)
-        self.setWindowTitle(self.title)
-        
         self.ui = Ui_DialogModelVisualization()
         self.ui.setupUi(self)
-        
-        self.setSignals()
-
-    def setSignals(self):
-        self.connect(self.ui.radioButtonWireframe,   QtCore.SIGNAL("toggled (bool)"),     self.setDisplayStyle)
-        self.connect(self.ui.radioButtonFlat,        QtCore.SIGNAL("toggled (bool)"),     self.setDisplayStyle)
-        self.connect(self.ui.radioButtonSmooth,      QtCore.SIGNAL("toggled (bool)"),     self.setDisplayStyle)
-        self.connect(self.ui.checkBoxBoundingBox,    QtCore.SIGNAL("toggled (bool)"),     self.viewer.setBoundingBox)
-        self.connect(self.ui.checkBoxModelVisible,   QtCore.SIGNAL("toggled (bool)"),     self.viewer.setModelVisibility)
-        self.connect(self.ui.pushButtonModelColor,   QtCore.SIGNAL("colorChanged ()"),    self.setModelColor)
-        self.connect(self.ui.pushButtonCenter,       QtCore.SIGNAL("pressed ()"),         self.viewer.emitViewerSetCenterLocal)
-        self.connect(self.ui.pushButtonClose,        QtCore.SIGNAL("pressed ()"),         self.viewer.unload)
-        self.connect(self.ui.doubleSpinBoxSizeX,     QtCore.SIGNAL("editingFinished ()"), self.viewer.setScale)
-        self.connect(self.ui.doubleSpinBoxSizeY,     QtCore.SIGNAL("editingFinished ()"), self.viewer.setScale)
-        self.connect(self.ui.doubleSpinBoxSizeZ,     QtCore.SIGNAL("editingFinished ()"), self.viewer.setScale)
-        self.connect(self.ui.doubleSpinBoxLocationX, QtCore.SIGNAL("editingFinished ()"), self.viewer.setLocation)
-        self.connect(self.ui.doubleSpinBoxLocationY, QtCore.SIGNAL("editingFinished ()"), self.viewer.setLocation)
-        self.connect(self.ui.doubleSpinBoxLocationZ, QtCore.SIGNAL("editingFinished ()"), self.viewer.setLocation)
-                                                 
-    def setModelColor(self):
-        self.viewer.setModelColor(self.ui.pushButtonModelColor.color())
-    
-    def setDisplayStyle(self, dummy):
-        if(self.ui.radioButtonWireframe.isChecked()):
-            displayStyle = self.viewer.DisplayStyleWireframe
-        elif(self.ui.radioButtonFlat.isChecked()):
-            displayStyle = self.viewer.DisplayStyleFlat
-        elif(self.ui.radioButtonSmooth.isChecked()):
-            displayStyle = self.viewer.DisplayStyleSmooth
-        self.viewer.setDisplayStyle(displayStyle)
