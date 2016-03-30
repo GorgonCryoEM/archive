18668248cf75838c9f16c5e970bf39488afda614
shadow_walker <shadowwalkersb@gmail.com>
2016-03-30 08:48:58 -0500

    ui in shape and sphere

--- bin/Explorer/sphere.py
+++ bin/Explorer/sphere.py
@@ -1,4 +1,6 @@
+from PyQt4 import QtGui, QtCore, QtOpenGL
 from shape import Shape
+from ui_common import Ui_Common
 
 
 class Sphere(Shape):
@@ -12,6 +14,12 @@ class Sphere(Shape):
         self.setColor(120, 18, 80, 150)
         
         self.setLoc(30., 10., 10.)
+        
+#         self.ui = Ui_Common()
+#         self.ui.setupUi(self)
+#
+#         self.dock = QtGui.QDockWidget("Common: Sphere", self)
+#         main.addDockWidget(QtCore.Qt.RightDockWidgetArea, self.dock)
         
     def draw(self):
         self.setMaterials(self.color)
